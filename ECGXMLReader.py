95d4103db9471469f9f3783ba53a0b425f6a950f
Robert Avram <robert.avram.md@gmail.com>
2022-10-02 21:39:09 +0000

    Precommit hook updated files

--- ECGXMLReader.py
+++ ECGXMLReader.py
@@ -1,10 +1,8 @@
-import os
-import csv
 import array
 import base64
-import xmltodict
-import pdb 
+
 import numpy as np
+import xmltodict
 
 __author__ = "Will Hewitt"
 __credits__ = ["Will Hewitt"]
@@ -13,60 +11,61 @@ __maintainer__ = "Will Hewitt"
 __email__ = "me@hewittwill.com"
 __status__ = "Development"
 
-class ECGXMLReader:
-    """ Extract voltage data from a ECG XML file """
-    def __init__(self, path, augmentLeads=False):
-        try: 
-            with open(path, 'rb') as xml:
-                self.ECG = xmltodict.parse(xml.read().decode('utf8'))
-            
-            self.augmentLeads           = augmentLeads
-            self.path                   = path
 
-            self.PatientDemographics    = self.ECG['RestingECG']['PatientDemographics']
-            self.TestDemographics       = self.ECG['RestingECG']['TestDemographics']
-            self.RestingECGMeasurements = self.ECG['RestingECG']['RestingECGMeasurements']
-            self.Waveforms              = self.ECG['RestingECG']['Waveform']
-            self.Diagnosis            = self.ECG['RestingECG']['Diagnosis']
+class ECGXMLReader:
+    """Extract voltage data from a ECG XML file"""
+
+    def __init__(self, path, augmentLeads=False):
+        try:
+            with open(path, "rb") as xml:
+                self.ECG = xmltodict.parse(xml.read().decode("utf8"))
+
+            self.augmentLeads = augmentLeads
+            self.path = path
+
+            self.PatientDemographics = self.ECG["RestingECG"]["PatientDemographics"]
+            self.TestDemographics = self.ECG["RestingECG"]["TestDemographics"]
+            self.RestingECGMeasurements = self.ECG["RestingECG"]["RestingECGMeasurements"]
+            self.Waveforms = self.ECG["RestingECG"]["Waveform"]
+            self.Diagnosis = self.ECG["RestingECG"]["Diagnosis"]
             try:
-                self.OriginalDiagnosis      = self.ECG['RestingECG']['OriginalDiagnosis']   
+                self.OriginalDiagnosis = self.ECG["RestingECG"]["OriginalDiagnosis"]
             except:
-                self.OriginalDiagnosis      = None
-   
-            
-            #pdb.set_trace()
-            
-            self.LeadVoltages           = self.makeLeadVoltages()
-        
+                self.OriginalDiagnosis = None
+
+            # pdb.set_trace()
+
+            self.LeadVoltages = self.makeLeadVoltages()
+
         except Exception as e:
             print(str(e))
-    
+
     def makeLeadVoltages(self):
 
         num_leads = 0
 
         leads = {}
 
-        for lead in self.Waveforms['LeadData']:
+        for lead in self.Waveforms["LeadData"]:
             num_leads += 1
-            
-            lead_data = lead['WaveFormData']
-            lead_b64  = base64.b64decode(lead_data)
-            lead_vals = np.array(array.array('h', lead_b64))
 
-            leads[ lead['LeadID'] ] = lead_vals
-        
+            lead_data = lead["WaveFormData"]
+            lead_b64 = base64.b64decode(lead_data)
+            lead_vals = np.array(array.array("h", lead_b64))
+
+            leads[lead["LeadID"]] = lead_vals
+
         if num_leads == 8 and self.augmentLeads:
 
-            leads['III'] = np.subtract(leads['II'], leads['I'])
-            leads['aVR'] = np.add(leads['I'], leads['II'])*(-0.5)
-            leads['aVL'] = np.subtract(leads['I'], 0.5*leads['II'])
-            leads['aVF'] = np.subtract(leads['II'], 0.5*leads['I'])
-        
+            leads["III"] = np.subtract(leads["II"], leads["I"])
+            leads["aVR"] = np.add(leads["I"], leads["II"]) * (-0.5)
+            leads["aVL"] = np.subtract(leads["I"], 0.5 * leads["II"])
+            leads["aVF"] = np.subtract(leads["II"], 0.5 * leads["I"])
+
         return leads
 
     def getLeadVoltages(self, LeadID):
         return self.LeadVoltages[LeadID]
-    
+
     def getAllVoltages(self):
         return self.LeadVoltages
