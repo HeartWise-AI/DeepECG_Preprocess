ec67ac93a375b7e29baa3df5fc3b0011cad244da
anolinlapalme <114109263+anolinlapalme@users.noreply.github.com>
2022-11-14 15:19:50 -0500

    Update generate_ECG_plot.py

--- generate_ECG_plot.py
+++ generate_ECG_plot.py
@@ -1,13 +1,19 @@
 from pylab import rcParams
 import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+pd.options.display.max_columns = None
+import matplotlib.ticker as ticker
 
 rcParams['figure.figsize'] = 30, 30
 rcParams['xtick.bottom'] = rcParams['xtick.labelbottom'] = False
 rcParams['ytick.left'] = rcParams['ytick.labelleft'] = False
-def plot_a_lead(row_num=50):
+def plot_a_lead(row_num=200):
 
     #sample y to: 1 only allow 0.4 ms sampling rate 
     #             2 only plot 12 seconds of per lead
+
 
     lead_id_list = ['I','II','III', 'V1','V2','V3','V4','V5','V6', 'aVL','aVR','aVF']
 
@@ -18,10 +24,6 @@ def plot_a_lead(row_num=50):
 
     lead_dict = dict(zip(lead_id_list,[[] for i in range(len(lead_id_list))]))
 
-    #generate the lead activation
-    activation = [0] *5 + [10] * 50 + [0] * 5
-
-
     for lead in lead_id_list:
         y = data_set['Lead_Wavform_1_ID_{}'.format(lead)].iloc[row_num]
         if len(y) == 5000:
@@ -30,141 +32,82 @@ def plot_a_lead(row_num=50):
 
         lead_dict[lead] = list(y)
 
+    #generate the lead activation
+    activation = [0] *5 + [10] * 100 + [0] * 5
+
     #generate the pannels
+    pannel_1_y = [i + 50 for i in activation] + [(i/100) + 50 for i in lead_dict['I'][110:625]] + [(i/100) + 50 for i in lead_dict['aVR'][0:625]] + [(i/100) + 50 for i in lead_dict['V1'][0:625]] + [(i/100) + 50 for i in lead_dict['V4'][0:625]]
+    pannel_2_y = [i + 15 for i in activation] + [(i/100) + 15 for i in lead_dict['II'][110:625]] + [(i/100) + 15  for i in lead_dict['aVL'][0:625]] + [(i/100) + 15  for i in lead_dict['V2'][0:625]] + [(i/100) + 15  for i in lead_dict['V5'][0:625]]
+    pannel_3_y = [i - 15 for i in activation] + [(i/100) - 15 for i in lead_dict['III'][110:625]] + [(i/100) - 15 for i in lead_dict['aVF'][0:625]] + [(i/100) - 15 for i in lead_dict['V3'][0:625]] + [(i/100) - 15 for i in lead_dict['V6'][0:625]]
+    pannel_4_y = [i - 50 for i in activation] + [(i/100) - 50 for i in lead_dict['II'][110::]]
 
-    pannel_1_y = activation + [i/4.88 for i in lead_dict['I'][0:625]] + [i/4.88 for i in lead_dict['aVR'][0:625]] + [i/4.88 for i in lead_dict['V1'][0:625]] + [i/4.88 for i in lead_dict['V4'][0:625]]
-    pannel_2_y = activation + [i/4.88 for i in lead_dict['II'][0:625]] + [i/4.88 for i in lead_dict['aVL'][0:625]] + [i/4.88 for i in lead_dict['V2'][0:625]] + [i/4.88 for i in lead_dict['V5'][0:625]]
-    pannel_3_y = activation + [i/4.88 for i in lead_dict['III'][0:625]] + [i/4.88 for i in lead_dict['aVF'][0:625]] + [i/4.88 for i in lead_dict['V3'][0:625]] + [i/4.88 for i in lead_dict['V6'][0:625]]
-    pannel_4_y = activation + [i/4.88 for i in lead_dict['II']]
+    fig, ax = plt.subplots(figsize=(40, 40))
+    ax.minorticks_on()
 
-    minor_ticks = np.arange(0, 2500, 10)
-    major_ticks = np.arange(0, 2500, 50)
+ 
+    ax.vlines(110,-10,-20, label='III', linewidth=4)
+    ax.text(110, -10, 'III', fontsize=44)
 
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+    ax.vlines(625,-10,-20, label='aVF', linewidth=4)
+    ax.text(625, -10, 'aVF', fontsize=44)
+
+    ax.vlines(1250,-10,-20, label='V3', linewidth=4)
+    ax.text(1250, -10, 'V3', fontsize=44)
+
+    ax.vlines(1875,-10,-20, label='V6', linewidth=4)
+    ax.text(1875, -10, 'V6', fontsize=44)
+    
+    ax.vlines(110,10,20, label='II', linewidth=4)
+    ax.text(110, 20, 'II', fontsize=44)
+
+    ax.vlines(625,10,20, label='aVL', linewidth=4)
+    ax.text(625, 20, 'aVL', fontsize=44)
+
+    ax.vlines(1250,10,20, label='V2', linewidth=4)
+    ax.text(1250, 20, 'V2', fontsize=44)
+
+    ax.vlines(1875,10,20, label='V5', linewidth=4)
+    ax.text(1875, 20, 'V5', fontsize=44)
+
+    ax.vlines(110,45,55, label='I', linewidth=4)
+    ax.text(110, 55, 'I', fontsize=44)
+
+    ax.vlines(625,45,55, label='aVR', linewidth=4)
+    ax.text(625, 55, 'aVR', fontsize=44)
+
+    ax.vlines(1250,45,55, label='V1', linewidth=4)
+    ax.text(1250, 55, 'V1', fontsize=44)
+
+    ax.vlines(1875,45,55, label='V4', linewidth=4)
+    ax.text(1875, 55, 'V4', fontsize=44)
+
+    ax.vlines(110,-55,-45, label='II', linewidth=4)
+    ax.text(110, -45, 'II', fontsize=44)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(125))
+    ax.xaxis.set_minor_locator(ticker.MultipleLocator(25))
+
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(1))
+
+    ax.grid(ls='-', color='red', linewidth=4)
+    ax.grid(which="minor", ls=':', color='red', linewidth=3)
+
+    ax.axis([0-100, 2500+100, min(pannel_4_y)-10, max(pannel_1_y)+10])
 
     x = [pos for pos in range(0, len(pannel_1_y))]
-    ax1.plot(x,pannel_1_y,linewidth=0.7, color='#000000')
+    ax.plot(x,pannel_1_y,linewidth=3, color='#000000')
+    ax.plot(x,pannel_2_y,linewidth=3, color='#000000')
+    ax.plot(x,pannel_3_y,linewidth=3, color='#000000')
+    ax.plot(x,pannel_4_y,linewidth=3, color='#000000')
 
-    ax1.set_xticks(major_ticks)
-    ax1.set_xticks(minor_ticks, minor=True)
-
-    minor_ticks_y = np.arange(min(pannel_1_y), max(pannel_1_y), 1)
-    major_ticks_y = np.arange(min(pannel_1_y), max(pannel_1_y), 5)
-
-    ax1.set_yticks(major_ticks_y)
-    ax1.set_yticks(minor_ticks_y, minor=True)
-
-    # And a corresponding grid
-    ax1.grid(which='both')
-
-    # Or if you want different settings for the grids:
-    ax1.grid(which='minor', color='#515151', linestyle=':')
-    ax1.grid(which='major', color='#515151', linestyle='--')
-
-    #add labels for leads
-    ax1.vlines(60,10,-10, label='I')
-    ax1.text(60, 11, 'I', fontsize=22)
-
-    ax1.vlines(685,10,-10, label='aVR')
-    ax1.text(685, 11, 'aVR', fontsize=22)
-
-    ax1.vlines(1310,10,-10, label='V1')
-    ax1.text(1310, 11, 'V1', fontsize=22)
-
-    ax1.vlines(1935,10,-10, label='V4')
-    ax1.text(1935, 11, 'V4', fontsize=22)
-
-    ax2.plot(x,pannel_2_y, linewidth=.7,color='#000000')
-
-    ax2.set_xticks(major_ticks)
-    ax2.set_xticks(minor_ticks, minor=True)
-
-    # And a corresponding grid
-    ax2.grid(which='both')
-
-    # Or if you want different settings for the grids:
-    ax2.grid(which='minor', color='#515151', linestyle=':')
-    ax2.grid(which='major', color='#515151', linestyle='--')
-
-    #add labels for leads
-    ax2.vlines(60,10,-10, label='II')
-    ax2.text(60, 11, 'II', fontsize=22)
-
-    ax2.vlines(685,10,-10, label='aVL')
-    ax2.text(685, 11, 'aVL', fontsize=22)
-
-    ax2.vlines(1310,10,-10, label='V2')
-    ax2.text(1310, 11, 'V2', fontsize=22)
-
-    ax2.vlines(1935,10,-10, label='V5')
-    ax2.text(1935, 11, 'V5', fontsize=22)
-
-    minor_ticks_y = np.arange(min(pannel_2_y), max(pannel_2_y), 1)
-    major_ticks_y = np.arange(min(pannel_2_y), max(pannel_2_y), 5)
-
-    ax2.set_yticks(major_ticks_y)
-    ax2.set_yticks(minor_ticks_y, minor=True)
-
-    ax3.plot(x,pannel_3_y,linewidth=.7,color='#000000')
-
-    ax3.set_xticks(major_ticks)
-    ax3.set_xticks(minor_ticks, minor=True)
-
-    # And a corresponding grid
-    ax3.grid(which='both')
-
-    # Or if you want different settings for the grids:
-    ax3.grid(which='minor', color='#515151', linestyle=':')
-    ax3.grid(which='major', color='#000000', linestyle='--')
-
-    #add labels for leads
-    ax3.vlines(60,10,-10, label='III')
-    ax3.text(60, 11, 'III', fontsize=22)
-
-    ax3.vlines(685,10,-10, label='aVF')
-    ax3.text(685, 11, 'aVF', fontsize=22)
-
-    ax3.vlines(1310,10,-10, label='V3')
-    ax3.text(1310, 11, 'V3', fontsize=22)
-
-    ax3.vlines(1935,10,-10, label='V6')
-    ax3.text(1935, 11, 'V6', fontsize=22)
-
-    minor_ticks_y = np.arange(min(pannel_3_y), max(pannel_3_y), 1)
-    major_ticks_y = np.arange(min(pannel_3_y), max(pannel_3_y), 5)
-
-    ax3.set_yticks(major_ticks_y)
-    ax3.set_yticks(minor_ticks_y, minor=True)
-
-    ax4.plot(x,pannel_4_y,linewidth=.7,color='#000000')
-
-    ax4.set_xticks(major_ticks)
-    ax4.set_xticks(minor_ticks, minor=True)
-
-    # And a corresponding grid
-    ax4.grid(which='both')
-
-    # Or if you want different settings for the grids:
-    ax4.grid(which='minor', color='#515151', linestyle=':')
-    ax4.grid(which='major', color='#000000', linestyle='--')
-
-    ax4.vlines(60,10,-10, label='II')
-    ax4.text(60, 11, 'II', fontsize=22)
-
-    minor_ticks_y = np.arange(min(pannel_4_y), max(pannel_4_y), 1)
-    major_ticks_y = np.arange(min(pannel_4_y), max(pannel_4_y), 5)
-
-    ax4.set_yticks(major_ticks_y)
-    ax4.set_yticks(minor_ticks_y, minor=True)
-
-    fig.suptitle(re.sub("\s\s+" , " ",data_set["Diag"].iloc[row_num].replace("ECG anormal","")), fontsize=12, y=0.99,  backgroundcolor='white')
+    ax.set_title(re.sub("\s\s+" , " ",data_set["Diag"].iloc[row_num].replace("ECG anormal","")), fontsize=20, y=0.96,  backgroundcolor='white')
     #plt.subplots_adjust(top=0.85)
 
     #add grid
     plt.tight_layout()
 
-    # shift subplots down:
-    plt.subplots_adjust(wspace=0, hspace=0)
+    #plt.savefig("/volume/sexy_{}_{}_.jpg".format(data_set['RestingECG_PatientDemographics_PatientID'].iloc[row_num],data_set['RestingECG_TestDemographics_AcquisitionDate'].iloc[row_num]))
+    
 
-    plt.savefig("/volume/corrected_{}_{}.jpg".format(data_set['RestingECG_PatientDemographics_PatientID'].iloc[row_num],data_set['RestingECG_TestDemographics_AcquisitionDate'].iloc[row_num]))
-
+plot_a_lead(row_num=200)
