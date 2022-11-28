3d7d9cc3ed07301267823fbcd056bf691a3b2ff8
anolinlapalme <114109263+anolinlapalme@users.noreply.github.com>
2022-11-27 23:36:55 -0500

    Delete main_preprocess.py

--- main_preprocess.py
+++
@@ -1,8 +0,0 @@
-import util_preprocess
-
-def generate_balanced_dataset(directory="/media/data1/muse_ge/ecg_retrospective"):
-  df = tinyxml2df("/media/data1/muse_ge/ecg_retrospective").read2flatten()
-  data_set_with_leads = TinyGetWaveform(df).generate_dataset()
-  train_x_,val_x_,test_x_, y_train,y_val,y_test= balanced_w_age_sex(data_set)
-
-  return train_x_,val_x_,test_x_, y_train,y_val,y_test
